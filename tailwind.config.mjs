812792243bbc4c847d4f34141d57a78663786784
hyperoot <hyperoot.tech@proton.me>
2024-07-20 16:13:41 +0000

    The last commit was a joke, this one is the real deal.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   prefix: "",
   theme: {
     container: {
