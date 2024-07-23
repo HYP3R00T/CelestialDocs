e2a6e2eac5eb9c52d96823bd26338aa334f8ba79
hyperoot <hyperoot.tech@proton.me>
2024-07-23 09:41:54 +0000

    minor changes to styling

--- src/components/docs/SideNavMenu.tsx
+++ src/components/docs/SideNavMenu.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 // Define the type for menu items
 interface MenuItem {
   title?: string;
@@ -13,31 +11,29 @@ interface SideNavMenuProps {
   level?: number;
 }
 
-const SideNavMenu: React.FC<SideNavMenuProps> = ({ items, level = 0 }) => (
-  <ul
-    className={`list-none ${
-      level > 0 ? "pl-4" : ""
-    } space-y-1 bg-green-300 m-0 p-4`}
-  >
-    {items.map((item) => {
-      const label = item.title ? item.title : item.slug.split("/").pop();
-      return (
-        <li key={item.slug} className="py-1">
-          <a
-            href={`/docs/${item.slug}`}
-            className={`text-foreground hover:text-accent-foreground ${
-              level === 0 ? "font-bold" : ""
-            }`}
-          >
-            {label}
-          </a>
-          {item.children.length > 0 && (
-            <SideNavMenu items={item.children} level={level + 1} /> // Pass down the level
-          )}
-        </li>
-      );
-    })}
-  </ul>
-);
+function SideNavMenu({ items, level = 0 }: SideNavMenuProps) {
+  return (
+    <ul className={`list-none m-0`}>
+      {items.map((item) => {
+        const label = item.title ? item.title : item.slug.split("/").pop();
+        return (
+          <li key={item.slug}>
+            <a
+              href={`/docs/${item.slug}`}
+              className={`text-foreground hover:text-accent-foreground ${
+                level === 0 ? "font-bold" : ""
+              }`}
+            >
+              {label}
+            </a>
+            {item.children.length > 0 && (
+              <SideNavMenu items={item.children} level={level + 1} />
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 export default SideNavMenu;
