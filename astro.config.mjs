4fb3f65b11e4d6cce261c143bf53889562b8759d
hyperoot <hyperoot.tech@proton.me>
2024-07-31 12:37:02 +0000

    it's time to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://celestial.hyperoot.dev",
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
