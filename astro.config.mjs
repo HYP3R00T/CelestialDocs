0705b53ce557c65b2ca189154de539d47f2f0c69
hyperoot <rajesh@hyperoot.dev>
2025-12-06 08:14:21 +0000

    refactor(config): integrate React into Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
